67c558b124efa9bd3ebdfc08da477f86978a0b33
jpileto <jp.ileto@icloud.com>
2017-03-29 22:33:26 -0400

    working on exercise 4

--- collections-n-iterations/exercise1.rb
+++ collections-n-iterations/exercise1.rb
@@ -18,3 +18,15 @@ movies = {Star_Wars: 1977, Transformers: 2007, Invictus: 2009}
 cities = {Toronto: 36286245, Manila: 57470097, New_York: 19795791}
 
 siblings = {Roofus: 12, Jeanne: 33, Faith: 21}
+
+puts fave_artists.first(2)
+
+movies.each do |title, year|
+  puts "#{title} came out in #{year}."
+end
+
+puts siblings.sort_by {|name, age| age}
+
+reversed_siblings = Hash[siblings.to_a.reverse]
+
+puts "#{reversed_siblings}"
